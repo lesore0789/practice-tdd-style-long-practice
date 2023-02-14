4808918190ce438dd117f5e8774853231bb0ce06
Leslie <lesore0789@gmail.com>
2023-02-13 18:55:07 -0800

    Completed phase 2 specs and code

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,9 +1,12 @@
 function returnsThree() {
-  // Your code here
+  return 3;
 }
 
 function reciprocal(n) {
-  // Your code here
+  if(n < 1 || n > 1000000){
+    throw new TypeError("Input needs to be between 1 and 1,000,000")
+  }
+  return 1/n;
 }
 
 module.exports = {
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,24 @@
-// Your code here
+const {expect} = require('chai');
+
+const { returnsThree, reciprocal } = require('../problems/number-fun');
+
+describe("returnsThree", () => {
+  let output = returnsThree()
+  it('Should return 3', () => {
+    expect(output).to.equal(3)
+  })
+});
+
+describe("reciprocal", () => {
+  let rec1 = reciprocal(5);
+  let rec2 = reciprocal(8);
+  it('Should return reciprocal of the input', () => {
+    expect(rec1).to.equal(1 / 5);
+    expect(rec2).to.equal(1 / 8);
+  })
+
+  let badInput = () => {reciprocal(-17)}
+  it('Should only take inputs between 1 and 1,000,000', () => {
+    expect(badInput).to.throw(TypeError, "Input needs to be between 1 and 1,000,000")
+  })
+});
